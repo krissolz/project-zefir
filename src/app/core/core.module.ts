426c71b9c0cca8da253159d1e1eddc501db86af3
krissolz <kristiyan.lazarov@gmail.com>
2024-02-26 16:43:43 +0200

    styling application

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { DATE_PIPE_DEFAULT_OPTIONS } from "@angular/common";
+import { DatePipe } from '@angular/common';
 import { AppRoutingModule } from '../app-routing.module';
 
 import { IndexService } from './index';
@@ -10,13 +10,9 @@ import { IndexService } from './index';
   ],
   imports: [
     AppRoutingModule,
+    DatePipe
   ],
-  providers: [
-    {
-      provide: DATE_PIPE_DEFAULT_OPTIONS,
-      useValue: { dateFormat: "longDate" }
-    }
-  ],
+  providers: [],
   exports:[
     ...IndexService
   ]
--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -3,14 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
 
-  currentDate: number;
+  public currentDate: string;
+  public contact: string;
 
   constructor() {
-    this.currentDate = Date.now();
+    this.currentDate = Date.now().toString();
+    this.contact = 'info@bookstore.com';
   }
 
   ngOnInit() {
